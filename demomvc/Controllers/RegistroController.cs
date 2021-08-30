345886cbdb7e7dd21d9d00183023f59daccf23f5
Edwin Ramos <edwin_ramos2@usmp.pe>
2021-08-29 19:36:39 -0500

    Creacion de editar y eliminar

--- demomvc/Controllers/RegistroController.cs
+++ demomvc/Controllers/RegistroController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using demomvc.Models;
 using demomvc.Data;
+using System.Linq;
 
 namespace demomvc.Controllers
 {
@@ -15,16 +16,47 @@ namespace demomvc.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View(_context.DataProductos.ToList());
         }
+         public IActionResult Create()
+        {
+            return View();
+        } 
 
+        [HttpPost]
         public IActionResult Create( Registro objProducto)
         {
             _context.Add(objProducto);
             _context.SaveChanges();
-            ViewData["Message"] = "El producto ha sido registrado";
+            ViewData["Message"] = "El producto ha sido registrado!!!";
 
-            return View("Index");
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Registro objProducto = _context.DataProductos.Find(id);
+            if(objProducto==null){
+                return NotFound();
+            }
+            return View(objProducto);
+        } 
+
+        [HttpPost]
+         public IActionResult Edit(int id,[Bind("id,producto,nombre,categoria,precio,descuento")] Registro objProducto)
+         {
+             _context.Update(objProducto);
+             _context.SaveChanges();
+              ViewData["Message"] = "El contacto ha sido actualizado!!!";
+             return View(objProducto);
+         }
+
+         public IActionResult Delete(int id)
+        {
+            Registro objProducto = _context.DataProductos.Find(id);
+            _context.DataProductos.Remove(objProducto);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
